181fe0c1257bd809a9efd6bfed7584e5645830d9
Brendan Connell <bmoney312@gmail.com>
2026-09-09 12:17:03 -0700

    connect to db using helper functions

--- build_results_sql_new.py
+++ build_results_sql_new.py
@@ -1,11 +1,10 @@
 import os
 import sys
 import json
-import pymysql
 import logging
 import urllib.request
 from html.parser import HTMLParser
-from lotw import get_current_week, get_current_year, get_all_games, response
+from lotw import get_current_week, get_current_year, get_all_games, response, get_db_connection
 
 # global variables
 logger = logging.getLogger()
@@ -196,20 +195,11 @@ def lambda_handler(event, context):
     if request_type is None:
         request_type = "manual_run"
 
-    db_endpoint = os.environ['db_endpoint']
-    db_port = int(os.environ['db_port'])
-    db_username = os.environ['db_username']
-    db_password = os.environ['db_password']
-    db_name = os.environ['db_name']
-
-    logger.info("Connecting to MySQL database {}".format(db_endpoint))
-
+    # connect to lotw database
     try:
-        conn = pymysql.connect(host=db_endpoint, port=db_port,
-                               user=db_username, passwd=db_password,
-                               db=db_name, connect_timeout=5)
+        conn = get_db_connection()
     except Exception as e:
-        logger.error("ERROR: Unexpected error: Could not connect to MySQL database - {}".format(str(e)))
+        logger.error("ERROR: Could not connect to MySQL database: {}".format(str(e)))
         sys.exit()
 
     logger.info("SUCCESS: Connection to MySQL database succeeded")
