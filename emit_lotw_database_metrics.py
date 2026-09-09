ff1b4b62135c70d357a77712102c907cd81f47f3
Brendan Connell <bmoney312@gmail.com>
2026-09-09 13:03:15 -0700

    connect to db using helper functions

--- emit_lotw_database_metrics.py
+++ emit_lotw_database_metrics.py
@@ -1,12 +1,10 @@
-import os
 import sys
 import json
-import pymysql
 import logging
 import datetime
 # import boto3
 from lotw import get_all_paid_players, get_all_players, get_all_current_players, get_all_picks
-from lotw import get_current_week, response, build_html
+from lotw import get_current_week, response, build_html, get_db_connection
 
 # global variables
 logger = logging.getLogger()
@@ -25,20 +23,11 @@ def lambda_handler(event, context):
         logger.error("Unable to determine request type")
         sys.exit()
 
-    db_endpoint = os.environ['db_endpoint']
-    db_port = int(os.environ['db_port'])
-    db_username = os.environ['db_username']
-    db_password = os.environ['db_password']
-    db_name = os.environ['db_name']
-
-    logger.info("Connecting to MySQL database {}".format(db_endpoint))
-
+    # create lotw database connection
     try:
-        conn = pymysql.connect(host=db_endpoint, port=db_port,
-                               user=db_username, passwd=db_password,
-                               db=db_name, connect_timeout=5)
+        conn = get_db_connection()
     except Exception as e:
-        logger.error("ERROR: Unexpected error: Could not connect to MySQL database - {}".format(str(e)))
+        logger.error("ERROR: Could not connect to MySQL database: {}".format(str(e)))
         sys.exit()
 
     logger.info("SUCCESS: Connection to MySQL database succeeded")
